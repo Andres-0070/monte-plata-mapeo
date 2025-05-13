8094abeff71f2ef9e19f4cf84073d54fff01c966
Andrés Mejía <andresgarrido1146t@gmail.com>
2025-05-13 11:46:51 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function abrirProvincia(areaElement) {
+    const nombreProvincia = areaElement.getAttribute('data-provincia');
+    
+    // Ruta destino (puede personalizarse según estructura)
+    const url = `${monte-plata}.html`;
+  
+    // Redirecciona a la página correspondiente
+    window.location.href = url="http://127.0.0.1:5500/monte-plata.html";
+  }
+  
